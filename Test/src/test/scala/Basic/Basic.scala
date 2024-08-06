ff359a34576c5be01b80f32490f1db6e4e8c8c8a
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-08-06 13:44:08 +0100

    Added a scala file.

---
+++ Test/src/test/scala/Basic/Basic.scala
@@ -0,0 +1,19 @@
+package Basic
+
+object Basic {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+
+  //Language fundamentals
+  var x = 10
+  println(x)
+  x = 11
+  println(x)
+
+  val y = 10
+//  y = 11
+
+  val z:Double = 10
+  println(z)
+}
